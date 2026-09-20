756b663943dfd34359e96ae5b797d25a6f369340
Adeilton Gomes <adeilton.accerttech@gmail.com>
2026-09-20 17:04:37 -0300

    feat(clima): intensidade real da chuva (garoa/moderada/forte) + raio na tempestade
    Antes toda chuva (garoa a temporal) usava o mesmo efeito fraco. Agora classify() distingue 3 niveis pelo weathercode do Open-Meteo e escala quantidade de gotas, velocidade, opacidade e vento; chuva forte ganha um veu escurecido na tela e codigos de trovoada (95/96/99) ganham flash de raio aleatorio. Cache local renomeado (v2) e TTL reduzido pra 10min. Bump do cache do service worker (v6).

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pelada-v5';
+const CACHE = 'pelada-v6';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/peladaecompromisso/'])));
   self.skipWaiting();
