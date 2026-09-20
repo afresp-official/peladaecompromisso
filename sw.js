e53b17a64dea897285461bb4d6d59b83bb8312e5
Adeilton Gomes <adeilton.accerttech@gmail.com>
2026-09-20 16:59:49 -0300

    feat(jogo): opcao de cancelar jogo sem excluir
    Jogo cancelado (ex: chuva) continua aparecendo no historico, marcado com selo vermelho e motivo opcional. latestGame/nextGame passam a pular jogos cancelados, entao confirmar presenca/campo/votacao nao ficam presos num jogo que nao aconteceu. Bump do cache do service worker (v5).

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pelada-v4';
+const CACHE = 'pelada-v5';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/peladaecompromisso/'])));
   self.skipWaiting();
