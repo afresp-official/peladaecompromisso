1e9542cbce70ee5d78aeddaf2b0a70feace99992
Adeilton Gomes <adeilton.accerttech@gmail.com>
2026-09-21 11:38:20 -0300

    fix: dia da semana fixo em 'Terca' no card da home e no historico
    Esses dois lugares tinham o texto 'Terca' escrito direto no HTML (sobra de quando so existia a AFRESP, que sempre joga as tercas). Agora usam weekdayLabel(game.date), que calcula o dia da semana real a partir da data cadastrada - necessario pra clubes que jogam em outro dia.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pelada-v22';
+const CACHE = 'pelada-v23';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/peladaecompromisso/'])));
   self.skipWaiting();
