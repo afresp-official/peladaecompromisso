1fa8106d39287cf1cf3bb8f17499115815889ea4
Adeilton Gomes <adeilton.accerttech@gmail.com>
2026-09-21 11:35:24 -0300

    fix(master-admin): cor do time invisivel no dropdown
    O <select> de cor no editor de times do Painel Master nao definia background para as <option>, entao o navegador usava o fundo branco padrao com texto branco herdado (invisivel). Adicionado style inline escuro nas options.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pelada-v21';
+const CACHE = 'pelada-v22';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/peladaecompromisso/'])));
   self.skipWaiting();
